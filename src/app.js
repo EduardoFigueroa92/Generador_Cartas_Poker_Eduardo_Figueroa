fcd210a2096e6684c0ecac44be9be25f6ed2bb03
Eduardo Figueroa <156800901+EduardoFigueroa92@users.noreply.github.com>
2024-03-20 15:13:46 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,80 @@
 /* eslint-disable */
-import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
+window.onload = () => {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  //document.getElementById("card").classList.add(generadorPalosPoker());
+  document.getElementById("boton").addEventListener("click", () => {
+    // Obtención de palo y de numero
+    let palo = generadorPalosPoker();
+    let numero = generadorNumerosPoker();
+
+    //Definicion del color en funcion del palo
+    let color = null; // inicializar la variable sin valor
+    if (palo == "♦" || palo == "♥") {
+      color = "style='color:red;'"; // la variable adquiere el texto de estilo para rojo
+    } else {
+      color = "style='color:black;'"; // la variable adquiere el texto de estilo para negro
+    }
+
+    // Construccion del html
+    document.getElementById("card").innerHTML =
+      "<p class= 'numArriba'" +
+      color +
+      ">" +
+      numero +
+      "</p>" +
+      "<p class= 'paloArriba'" +
+      color +
+      ">" +
+      palo +
+      "</p>" +
+      "<div class= 'recuadro'>" +
+      "<p class= 'numero'" +
+      color +
+      ">" +
+      numero +
+      "</p>" +
+      "</div>" +
+      "<p class= 'paloAbajo'" +
+      color +
+      ">" +
+      palo +
+      "</p>" +
+      "<p class= 'numAbajo'" +
+      color +
+      ">" +
+      numero +
+      "</p>";
+  });
+
+  console.log(document.getElementById("card"));
+  console.log(generadorPalosPoker());
+};
+
+const generadorPalosPoker = () => {
+  let palos = ["♦", "♥", "♣", "♠"];
+  let indicePalos = Math.floor(Math.random() * palos.length);
+  return palos[indicePalos];
+};
+
+const generadorNumerosPoker = () => {
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
+  let indiceNumeros = Math.floor(Math.random() * numeros.length);
+
+  return numeros[indiceNumeros];
 };
